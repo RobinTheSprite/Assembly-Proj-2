602b5c79a5ac33a225cb9ffe75cca39c97a56b26
RobinTheSprite <madscientistak@gmail.com>
2017-12-03 22:54:59 -0900

    Hopefully the presentation version

---
+++ midi-like/midi-like/codebreaker.ml
@@ -0,0 +1,2 @@
+[R]kick-electro02 16R 16R 16R 16R 16R 16R 16R 16R 16R 16R 16R 16R 16R 16R 16R 16R 16R 16R 16R 16R 16R
+[M]PureSine       04E     04D    04C    04D    08E -- 08E -- 02E           08D -- 08D -- 02D           08E -- 08G -- 02G
---
+++ midi-like/midi-like/immigrantsong.ml
@@ -0,0 +1,5 @@
+[R]Bass-Drum-2        	  08R 16R 16R - 08R   -   08R 16R 16R - 04R   -	
+[R]Ensoniq-VFX-SD-Snare-2 -- 	--    08R   - 04R      -      08R   - 04R
+[R]Closed-Hi-Hat-1	  --    08R   08R   08R   08R   08R   08R   08R   08R   08R
+[R]Crash-Cymbal-1	  00R
+![M]
